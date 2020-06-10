0239c8fb63c07d2f03493885eedfb9ea45c271d0
Adam <adam.filipowicz92@gmail.com>
2020-06-10 17:17:34 +0200

    Delete run_master.sh

--- run_master.sh
+++
@@ -1,8 +0,0 @@
-gcloud beta firebase test android run \
-  --type instrumentation \
-  --app app-debug.apk \
-  --test app-multiple-success-debug-androidTest.apk \
-  --device model=NexusLowRes,version=28,locale=en,orientation=portrait \
-  --test-targets-for-shard "class com.example.test_app.ParameterizedTest#shouldHopefullyPass"
-
-java -jar ../flank-master.jar firebase test android run flank.yml
